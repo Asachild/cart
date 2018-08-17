0081388f47e1668e971e41efa80bf2e5da16a88f
王雪 <xianghaiziyiyang@126.com>
2018-08-17 09:36:44 +0800

    Add files via upload

---
+++ js/public.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+	var scale = 1 / devicePixelRatio;
+	document.documentElement.setAttribute ( 'data-dpr' , devicePixelRatio ) ;
+	document.querySelector('meta[name="viewport"]').setAttribute('content','initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+	document.documentElement.style.fontSize = document.documentElement.clientWidth / 10 + 'px';
+}
